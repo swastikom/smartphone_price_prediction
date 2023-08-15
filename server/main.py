3aff673cd9b50e9d7ff055d1b8ab1229ce6fac1c
Swastik Dhar <swastikdhar18@gmail.com>
2023-08-16 00:05:27 +0530

    server issue solving...

--- server/main.py
+++ server/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
 from routes.prediction import router as prediction_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",  # Add your frontend's origin here
+    # Add more origins as needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(prediction_router)
