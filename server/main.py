f56de6173e03364b43dac959aedf4256fffb0bd4
Swastik Dhar <swastikdhar18@gmail.com>
2023-08-17 22:07:54 +0530

    server fix...

--- server/main.py
+++ server/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 origins = [
     "http://localhost:3000",  # Add your frontend's origin here
-    "https://smartphone-price-prediction.vercel.app/"
+    "https://smartphone-price-prediction.vercel.app"
 ]
 
 app.add_middleware(
